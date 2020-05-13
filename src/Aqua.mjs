cf4bcf4328950e491873baca425dfe9e9979dd74
Bryan Morrow <bkmorrow@gmail.com>
2020-05-13 09:41:33 -0500

    Added listVulnerabilities
    * listVulnerabilities gets all vulnerabilities
    * getRemaining is a helper function to calculate the remaining items

--- src/Aqua.mjs
+++ src/Aqua.mjs
@@ -3,88 +3,88 @@ import https from 'https'
 
 
 
-class Aqua{
+class Aqua {
 
-  constructor(userId, password, host, port = 443, useTLS = true, verifyTLS = false, apiVersion = 'v1', remember = true){
-    this.userId = userId;
-    this.host = host;
-    this.port = port;
-    this.useTLS = useTLS;
-    this.verifyTLS = verifyTLS;
-    this.apiVersion = apiVersion;
-    this.remember = remember;
-    this.token = "";
-    this.baseURL = `http${useTLS ? 's' : ''}://${host}:${port}/api`
-    this.config = { headers: {'Content-Type': 'application/json'}};
-    this.password = password;
-
-
-    this._axiosInstance = axios.create({
-      baseURL: this.baseURL,
-      responseType: "json",
-      httpsAgent: new https.Agent({  
-        rejectUnauthorized: this.verifyTLS
-      }),
-    })
+    constructor(userId, password, host, port = 443, useTLS = true, verifyTLS = false, apiVersion = 'v1', remember = true) {
+        this.userId = userId;
+        this.host = host;
+        this.port = port;
+        this.useTLS = useTLS;
+        this.verifyTLS = verifyTLS;
+        this.apiVersion = apiVersion;
+        this.remember = remember;
+        this.token = "";
+        this.baseURL = `http${useTLS ? 's' : ''}://${host}:${port}/api`
+        this.config = { headers: { 'Content-Type': 'application/json' } };
+        this.password = password;
 
 
-  }
+        this._axiosInstance = axios.create({
+            baseURL: this.baseURL,
+            responseType: "json",
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: this.verifyTLS
+            }),
+        })
 
 
-    async init(){
+    }
+
+
+    async init() {
         await this._axiosInstance.post('/v1/login', {
-            id: this.userId,
-            password: this.password,
-            remember: this.remember
-          })
-          .then(response => response.data)
-          .then(data => {
-            this.token = data.token
-            this.config.headers =  {...this.config.headers, 'Authorization': `Bearer ${this.token}` }
-          })
-          .catch((error) => {
-            console.log("in error")
-            console.log(error);
-          });
-          this.password = ""
+                id: this.userId,
+                password: this.password,
+                remember: this.remember
+            })
+            .then(response => response.data)
+            .then(data => {
+                this.token = data.token
+                this.config.headers = {...this.config.headers, 'Authorization': `Bearer ${this.token}` }
+            })
+            .catch((error) => {
+                console.log("in error")
+                console.log(error);
+            });
+        this.password = ""
     };
 
 
-    async containers(nodeId = "", groupBy = 'containers', status = 'running', page = '1', pageSize = '50'){
-      let containers = []
+    async containers(nodeId = "", groupBy = 'containers', status = 'running', page = '1', pageSize = '50') {
+        let containers = []
 
-      await this._axiosInstance.get('/v1/containers', this.config, {
-        nodeId,
-        groupBy,
-        status,
-        page,
-        pageSize
-      })
-      .then(response => {
-        containers = response.data
-      })
-      .catch( error => {
-        console.log(error)
-      });
+        await this._axiosInstance.get('/v1/containers', this.config, {
+                nodeId,
+                groupBy,
+                status,
+                page,
+                pageSize
+            })
+            .then(response => {
+                containers = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            });
 
-      return containers
+        return containers
 
     }
 
 
 
-    async listRegisteredImages({registry = null, repository = null, name = null, page = 1, pageSize = 50, orderBy = null}){
+    async listRegisteredImages({ registry = null, repository = null, name = null, page = 1, pageSize = 50, orderBy = null }) {
         let registeredImages = []
         let params = { registry, repository, name, page, page_size: pageSize, order_by: orderBy }
         this.config.params = params
 
         await this._axiosInstance.get('/v2/images', this.config)
-        .then(response => {
-          registeredImages = response.data
-        })
-        .catch( error => {
-          console.log(error)
-        });
+            .then(response => {
+                registeredImages = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            });
 
         this.config.params = {}
         return registeredImages
@@ -93,20 +93,43 @@ class Aqua{
 
 
     async listRegistries() {
-      let registries = []
+        let registries = []
 
-      await this._axiosInstance.get('/v1/registries', this.config)
-        .then(response => response.data)
-        .then( data => {
-          registries = data
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
+        await this._axiosInstance.get('/v1/registries', this.config)
+            .then(response => response.data)
+            .then(data => {
+                registries = data
+            })
+            .catch((error) => {
+                console.log(error.response);
+            });
 
         return registries;
     }
 
+    async listVulnerabilities(page = "", pageSize = "") {
+        let vulnerabilities = []
+        let params = { page: page, pagesize: pageSize }
+        this.config.params = params
+        await this._axiosInstance.get('v2/risks/vulnerabilities', this.config)
+            .then(response => {
+                vulnerabilities = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            });
+        this.config.params = {}
+        return vulnerabilities
+    }
+
+    // Helper functions
+    getRemaining(total, pagesize) {
+        let remaining = total - Number(pagesize)
+        if (remaining < 0) {
+            remaining = 0
+        }
+        return remaining
+    }
 }
 
 
